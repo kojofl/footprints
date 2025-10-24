3acc3205a9f379dd7b5cfc976f51e8e593bb388f
kojofl <korbinianfl98@googlemail.com>
2025-10-24 16:40:15 +0200

    feat: allow fullscreen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 	import "../app.css";
 	import { mode } from "$lib/mode_state.js";
+	import { getCurrentWindow } from "@tauri-apps/api/window";
 
 	let { children } = $props();
 	$effect(() => {
 		document.documentElement.setAttribute("data-mode", mode.current);
 	});
+
+	async function onKeyDown(e: KeyboardEvent) {
+		console.log(e.key);
+		if (e.key === "F11") {
+			const w = getCurrentWindow();
+			const set = await w.isFullscreen();
+			await w.setFullscreen(!set);
+		}
+	}
 </script>
 
 {@render children?.()}
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
